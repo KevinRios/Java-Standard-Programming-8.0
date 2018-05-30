65d4db99cf0a2333dcaeaf9771d6536267d6bffe
KevinRios <KevinJIRios@gmail.com>
2018-05-29 23:48:28 -0300

    Subida Integrador Fase 1

---
+++ Ejercicios/Integrador/src/integrador/Programa.java
@@ -0,0 +1,21 @@
+package integrador;
+
+
+public class Programa {
+
+    public static void main(String[] args) {
+        
+        
+        Vehiculo vec1 = new Vehiculo(15, 30, 45);
+        
+        Persona pers1 = new Persona("qwerty", "ASD", "11222333");
+        
+        
+        System.out.println(pers1);
+        System.out.println("------");
+        System.out.println(vec1);
+        
+        
+    }
+    
+}
