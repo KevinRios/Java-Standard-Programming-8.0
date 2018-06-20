f764cbaa57f8b75a4154b21210205d446a0fa820
KevinRios <KevinJIRios@gmail.com>
2018-06-20 14:12:58 -0300

    Clase10
    fin app con persistencia de datos (CRUD)

---
+++ Clase10/src/ar/com/KevinRios/java/colegio/Colegio.java
@@ -0,0 +1,28 @@
+
+package ar.com.KevinRios.java.colegio;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Colegio extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Colegio.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("App Colegio");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
